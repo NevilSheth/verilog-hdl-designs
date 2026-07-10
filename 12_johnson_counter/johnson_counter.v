db9d3ae734c6248f14a1dee1c1ca1d833ee82d38
NevilSheth <nevilsheth02@gmail.com>
2026-07-10 21:21:07 +0530

    Adding johnson counter

---
+++ 12_johnson_counter/johnson_counter.v
@@ -0,0 +1,16 @@
+module johnson_counter(clk,reset,q);
+
+input clk,reset;
+output reg[3:0]q;
+
+always@(posedge clk or posedge reset)
+
+begin
+if(reset)
+q<=4'b0000;
+else
+q<={~q[0],q[3:1]};
+end
+
+endmodule
+
